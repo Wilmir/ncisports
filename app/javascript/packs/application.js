b6706f44b3ac03d6c7214583874cfc5028b5fe5b
wilmir <wbnicanor01@gmail.com>
2020-04-27 13:59:33 +0100

    Updates the jquery version to 3.4.1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-import "bootstrap"
+import "bootstrap"    
+import "../stylesheets/application"
+
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
