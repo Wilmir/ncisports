f269a448a39ccd8b6722f5f5add15b594d670d35
wilmir <wbnicanor01@gmail.com>
2020-04-27 22:29:48 +0100

    Adds authorization to index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_customer, only: [:edit, :update]
+  before_action :logged_in_customer, only: [:index, :edit, :update]
   before_action :correct_customer,   only: [:edit, :update]
   before_action  :admin_user, only: :destroy
 
