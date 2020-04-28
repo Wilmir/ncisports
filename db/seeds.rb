6a3c4fc0a9f87f8f555c40a90abab163a1dedd4f
wilmir <wbnicanor01@gmail.com>
2020-04-28 12:39:56 +0100

    Adds funny description from IT crowd, Adds Rupaul characters as customers, Adds soccer leageus and teams as titles

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Customer.create!(name:  "Wilmir Nicanor",
 
 # Generate a bunch of additional users.
 20.times do |n|
-  name  = Faker::Name.unique.name
+  name  = Faker::TvShows::RuPaul.queen
   email = "customer-#{n+1}@ncisports.com"
   address = Faker::Address.full_address
   mobile_number = Faker::PhoneNumber.cell_phone
@@ -24,12 +24,29 @@ end
 
 # Generate a bunch of issues
 customers = Customer.order(:created_at).take(10)
-20.times do |n|
-    issue_type = "Refund"
-    status = Faker::Subscription.status
-    title = Faker::Sports::Football.competition
-    description = Faker::Quote.matz
-    customers.each do |customer|
+
+
+customers.each do |customer|
+    10.times do |n|
+        issue_type = "Refund"
+        issue_rand = rand(1..5)
+        
+        case issue_rand
+        when 1
+            issue_type = "Book A Game"
+        when 2
+            issue_type = "Cancel Booking"
+        when 3
+            issue_type = "Refund"
+        when 4
+            issue_type = "Feedback"
+        when 5
+            issue_type = "Other"
+        end
+
+        status = Faker::Subscription.status
+        title = Faker::Sports::Football.competition + " : " + Faker::Sports::Football.team + " vs "  + Faker::Sports::Football.team 
+        description = Faker::TvShows::TheITCrowd.quote
         customer.issues.create!(issue_type:  issue_type,
                                 status:  status,
                                 title: title,
