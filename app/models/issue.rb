483bb3342b8788df30f3070eb10e390e6645c8c9
wilmir <wbnicanor01@gmail.com>
2020-04-28 11:23:22 +0100

    Adds links to Cases cards

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,6 +3,6 @@ class Issue < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :customer_id, presence:true
   validates :issue_type, presence:true
-  validates :title, presence:true, length: {maximum: 100}
-  validates :description, presence:true
+  validates :title, presence:true, length: {maximum: 500}
+  validates :description, presence:true, length: {maximum: 10000}
 end
