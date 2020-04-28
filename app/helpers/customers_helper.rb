cdff33c4e5a0883c5d31cd2376e38963b8e2d184
wilmir <wbnicanor01@gmail.com>
2020-04-28 23:06:18 +0100

    Updates layout for customers viewing other customers

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,8 +1,8 @@
 module CustomersHelper
        # Returns the Gravatar for the given user.
-       def gravatar_for(customer, size: 80)
+    def gravatar_for(customer, size: 100)
         gravatar_id = Digest::MD5::hexdigest(customer.email.downcase)
         gravatar_url  = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-        image_tag(gravatar_url, alt: customer.name, class:"gravatar")
+        image_tag(gravatar_url, alt: customer.name, class:"gravatar rounded-circle")
     end
 end
